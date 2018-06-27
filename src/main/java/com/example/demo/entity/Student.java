441f233e63e60bd69401b92d7fb9adecffa1e173
Ali <alinorbobaev97@mail.ru>
2018-06-27 11:36:17 +0300

    исправленная сущность студент

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,12 +1,9 @@
 package com.example.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // This tells Hibernate to make a table out of this class
-
+@Table(name = "students")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,52 +19,62 @@ public class Student {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public int getAttendance() {
-        return attendance;
-    }
-
-    public int getAcperformance() {
-        return acperformance;
-    }
-
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    @Column(name = "lastname")
+    public String getLastname() {
+        return lastname;
+    }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    @Column(name = "course")
+
+    public String getCourse() {
+        return course;
     }
 
     public void setCourse(String course) {
         this.course = course;
     }
 
+    @Column(name = "specialty")
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
     }
 
+    @Column(name = "attendance")
+
+    public int getAttendance() {
+        return attendance;
+    }
+
     public void setAttendance(int attendance) {
         this.attendance = attendance;
+    }
+
+    @Column(name = "acperformance")
+
+    public int getAcperformance() {
+        return acperformance;
     }
 
     public void setAcperformance(int acperformance) {
